5f8d0aff0faaec5dbdef3e50dfa22c96d90760ef
DenisStepanidenko <stepanidenko.den@mail.ru>
2023-10-16 22:35:23 +0300

    new problems

---
+++ src/DFSRecursive/Solution.java
@@ -0,0 +1,58 @@
+package DFSRecursive;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+// алгоритм поиск в глубину
+public class Solution {
+    // будем реализовать через список смежности
+    public static void main(String[] args) {
+        // построим для примера граф
+        Map<Integer , List<Edge>> graph = new HashMap<>();
+        addEdge(graph , 0 , 1 , 6);
+        addEdge(graph , 1 , 2 , 7);
+        addEdge(graph , 2 , 3 , 10);
+        addEdge(graph , 3 , 1 , 4);
+        addEdge(graph , 1 , 4 , 6);
+        addEdge(graph , 4 , 5 , 12);
+        addEdge(graph , 0 , 7 , 9);
+        addEdge(graph , 0 , 6 , 10);
+        boolean[] visited = new boolean[8];
+        DFS(0 , visited , graph);
+    }
+    static class Edge{
+        int from;
+        int to;
+        int cost;
+
+        public Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+    public static void DFS(int at , boolean[] visited , Map<Integer , List<Edge>> graph){
+        // так как алгоритм рекурсивны, то проверяем, были ли мы уже в этой вершине
+        if(visited[at]) return;
+        visited[at] = true;
+
+        // мы находимся в данной вершине, значит нужно узнать список ребёр всех
+        List<Edge> current = graph.get(at);
+        if(current != null) {
+            for (Edge edge : current) {
+                // проходимся по всем ребрам далее
+                System.out.println("В данный момент мы находимся на ребре - " + edge.from + " " + edge.to + " " + edge.cost);
+                DFS(edge.to, visited, graph);
+            }
+        }
+    }
+
+    // вспомогательный метод для создания самого графа
+    public static void addEdge(Map<Integer , List<Edge>> graph , int from , int to , int cost){
+        List<Edge> current = graph.computeIfAbsent(from, k -> new ArrayList<Edge>());
+        current.add(new Edge(from , to , cost));
+    }
+}
---
+++ src/FindingConnectedComponentsDFS/Solution.java
@@ -0,0 +1,94 @@
+package FindingConnectedComponentsDFS;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<Integer , List<Edge>> graph = new HashMap<>();
+        Map<Integer , List<Integer>> connectivityComponents = new HashMap<>(); // здесь будут храниться найденные компоненты связности
+        addEdge(graph , 0 , 1 , 20);
+        addEdge(graph , 0 , 2 , 20);
+        addEdge(graph , 1 , 2 , 20);
+
+        addEdge(graph , 3 , 4 , 20);
+        addEdge(graph , 4 , 5 , 20);
+        addEdge(graph , 3 , 5 , 20);
+
+        addEdge(graph , 6 , 7 , 20);
+        addEdge(graph , 7 , 8 , 20);
+        addEdge(graph , 6 , 8 , 20);
+
+        addEdge(graph , 9 , 2 , 30);
+        addEdge(graph , 2, 9 , 30);
+
+        addEdge(graph , 10 , 5 , 20);
+        addEdge(graph , 5 , 10 , 30);
+        List<Boolean> visited = new ArrayList<>(11);
+        for(int i =0; i < 11; i++){
+            visited.add(false);
+        }
+        connectivityComponents = findConnectedComponents(graph , visited);
+        for (Map.Entry<Integer, List<Integer>> entry : connectivityComponents.entrySet()) {
+            System.out.println("------");
+            System.out.println("Компонента связности - " + entry.getKey());
+            List<Integer> allVertex = entry.getValue();
+            for (Integer vertex : allVertex) {
+                System.out.print(vertex + " ");
+            }
+            System.out.println("------");
+        }
+    }
+
+    static public Map<Integer , List<Integer>> findConnectedComponents(Map<Integer , List<Edge>> graph , List<Boolean> visited){
+        Map<Integer, List<Integer>> connectivityComponents = new HashMap<>();
+        int count = 0; // компоненты связности будем итерировать с 1
+        for(int i =0; i <visited.size(); i++){
+            if(!visited.get(i)){
+                count++;
+                connectivityComponents.put(count , new ArrayList<>());
+                dfs(graph , i , visited , count , connectivityComponents);
+            }
+        }
+        return connectivityComponents;
+    }
+
+    private static void dfs(Map<Integer, List<Edge>> graph, int i , List<Boolean> visited , int count , Map<Integer, List<Integer>> connectivityComponents) {
+        if(!visited.get(i)) {
+            visited.set(i, true); // уже
+            List<Integer> listVertex = connectivityComponents.get(count);
+            listVertex.add(i);
+            connectivityComponents.put(count, listVertex);
+        }
+        else{
+            return;
+        }
+        // то есть добавили текущую вершину в текущую компоненту связности
+        // нужно узнать вершины, которые соединены с текущей
+        List<Edge> currentVertexEdges = graph.get(i);
+        if(currentVertexEdges != null) {
+            for (Edge x : currentVertexEdges){
+                dfs(graph , x.to , visited , count , connectivityComponents);
+            }
+        }
+    }
+
+    // вспомогательный метод для графа
+    static public void addEdge(Map<Integer , List<Edge>> graph , int from , int to , int cost){
+        List<Edge> current = graph.computeIfAbsent(from , k-> new ArrayList<>());
+        current.add(new Edge(from , to , cost));
+    }
+    static class Edge{
+        int from;
+        int to;
+        int cost;
+
+        public Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+}
